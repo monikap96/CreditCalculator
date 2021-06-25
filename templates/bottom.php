496898b2a5274368109d653186d837fe433d91e6
monikap96 <monikap961@gmail.com>
2021-06-25 23:54:57 +0200

    Added top and bottom element

---
+++ templates/bottom.php
@@ -0,0 +1,31 @@
+<?php require_once dirname(__FILE__) .'/../config.php'; ?>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+						<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+						<li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; monikap96. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="<?php echo _APP_ROOT;?>/assets/js/jquery.min.js"></script>
+			<script src="<?php echo _APP_ROOT;?>/assets/js/jquery.scrolly.min.js"></script>
+			<script src="<?php echo _APP_ROOT;?>/assets/js/jquery.dropotron.min.js"></script>
+			<script src="<?php echo _APP_ROOT;?>/assets/js/jquery.scrollex.min.js"></script>
+			<script src="<?php echo _APP_ROOT;?>/assets/js/browser.min.js"></script>
+			<script src="<?php echo _APP_ROOT;?>/assets/js/breakpoints.min.js"></script>
+			<script src="<?php echo _APP_ROOT;?>/assets/js/util.js"></script>
+			<script src="<?php echo _APP_ROOT;?>/assets/js/main.js"></script>
+
+	</body>
+</html>
